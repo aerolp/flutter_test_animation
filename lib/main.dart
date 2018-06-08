ca8d7d970dbd92b760ade3b839ce5be85ba5c274
Victor Le Pen <victor@yarilabs.com>
2018-06-08 16:48:06 +0100

    add alert dialog

--- lib/main.dart
+++ lib/main.dart
@@ -1,109 +1,145 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(new MyApp());
+void main() {
+  runApp(new MaterialApp(
+    home: new AlertDialogTest(),
+  ));
+}
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class AlertDialogTest extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return new MaterialApp(
-      title: 'Flutter Demo',
-      theme: new ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or press Run > Flutter Hot Reload in IntelliJ). Notice that the
-        // counter didn't reset back to zero; the application is not restarted.
-        primarySwatch: Colors.blue,
+    return new Material(
+      child: new InkWell(
+        child: new Hero(
+          tag: "preview",
+          child: new Container(
+            alignment: FractionalOffset.bottomLeft,
+            child: new Image(
+              image: new AssetImage('images/h_pig.png'),
+            ),
+          ),
+        ),
+        onTap: () {
+          showDialog(
+            context: context,
+            child: new AlertDialog(
+              content: new Hero(
+                tag: "preview",
+                child: new ListView(
+                  children: <Widget>[
+                    new Image(image: new AssetImage('images/h_pig.png'),),
+                    new Text('O Lorem Ipsum é um texto modelo da indústria tipográfica e de impressão. '),
+                  ],  
+                ),
+              ),
+            ),
+          );
+        },
       ),
-      home: new MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => new _MyHomePageState();
+/*void main() {
+  runApp(new MaterialApp(
+    home: new HomePage(),
+  ));
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class HeroDialogRoute<T> extends PageRoute<T> {
+  HeroDialogRoute({ this.builder }) : super();
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  final WidgetBuilder builder;
+
+  @override
+  bool get opaque => false;
+
+  @override
+  bool get barrierDismissible => true;
+
+  @override
+  Duration get transitionDuration => const Duration(milliseconds: 300);
+
+  @override
+  bool get maintainState => true;
+
+  @override
+  Color get barrierColor => Colors.black54;
+
+  @override
+  Widget buildTransitions(BuildContext context, Animation<double> animation, Animation<double> secondaryAnimation, Widget child) {
+    return new FadeTransition(
+      opacity: new CurvedAnimation(
+        parent: animation,
+        curve: Curves.easeOut
+      ),
+      child: child
+    );
   }
 
   @override
+  Widget buildPage(BuildContext context, Animation<double> animation,
+    Animation<double> secondaryAnimation) {
+    return builder(context);
+  }
+
+}
+
+class HomePage extends StatelessWidget {
+  @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return new Scaffold(
       appBar: new AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: new Text(widget.title),
+        title: new Text('Hero demo'),
       ),
-      body: new Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: new Column(
-          // Column is also layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug paint" (press "p" in the console where you ran
-          // "flutter run", or select "Toggle Debug Paint" from the Flutter tool
-          // window in IntelliJ) to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            new Text(
-              'You have pushed the button this many times:',
+      body: new Align(
+        alignment: FractionalOffset.center,
+        child: new Card(
+          child: new Hero(
+            tag: 'developer-hero',
+            child: new Container(
+              width: 300.0,
+              height: 300.0,
+              child: new FlutterLogo(),
             ),
-            new Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
+          ),
         ),
       ),
       floatingActionButton: new FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: new Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        child: new Icon(Icons.developer_mode),
+        onPressed: () {
+          Navigator.push(
+            context,
+            new HeroDialogRoute(
+              builder: (BuildContext context) {
+                return new Center(
+                  child: new AlertDialog(
+                    title: new Text('You are my hero.'),
+                    content: new Container(
+                      child: new Hero(
+                        tag: 'developer-hero',
+                        child: new Container(
+                          height: 200.0,
+                          width: 200.0,
+                          child: new FlutterLogo(),
+                        ),
+                      ),
+                    ),
+                    actions: <Widget>[
+                      new FlatButton(
+                        child: new Text('RAD!'),
+                        onPressed: Navigator
+                          .of(context)
+                          .pop,
+                      ),
+                    ],
+                  ),
+                );
+              },
+            ),
+          );
+        },
+      ),
     );
   }
-}
+}*/
